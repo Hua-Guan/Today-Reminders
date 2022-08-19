7d5ac2097a6226c3dd394e1cf7ee807e14fb0b08
HuaGuan <G18174078632@gmail.com>
2022-08-19 11:06:09 +0800

    更新
    增加点击取消事项的功能

--- app/src/main/java/pri/guanhua/todayreminders/model/ListViewAdapter.java
+++ app/src/main/java/pri/guanhua/todayreminders/model/ListViewAdapter.java
@@ -1,6 +1,11 @@
 package pri.guanhua.todayreminders.model;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.LayoutInflater;
@@ -10,12 +15,16 @@ import android.widget.BaseAdapter;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+
 import java.util.List;
 
+import pri.guanhua.todayreminders.GlobalValues;
 import pri.guanhua.todayreminders.R;
 import pri.guanhua.todayreminders.model.bean.ThingsBean;
 import pri.guanhua.todayreminders.model.bean.ThingsEntity;
 import pri.guanhua.todayreminders.model.database.AppDatabase;
+import pri.guanhua.todayreminders.view.AddThingsActivity;
 
 public class ListViewAdapter extends BaseAdapter {
 
@@ -82,6 +91,7 @@ public class ListViewAdapter extends BaseAdapter {
             @Override
             public void onClick(View v) {
                 new Thread(new Runnable() {
+                    @RequiresApi(api = Build.VERSION_CODES.S)
                     @Override
                     public void run() {
                         AppDatabase instance = AppDatabase.getInstance(mContext);
@@ -89,6 +99,20 @@ public class ListViewAdapter extends BaseAdapter {
                         long id = thingsBean.id;
                         instance.thingsDao().deleteById(Integer.parseInt(Long.toString(id)));
                         mList.remove(thingsBean);
+                        //取消闹钟通知
+                        Intent intent = new Intent();
+                        ComponentName name = new ComponentName(GlobalValues.PACKAGE_NAME, GlobalValues.ALARM_CLASS_PATH);
+                        intent.setAction(GlobalValues.TIME_ACTION);
+                        intent.putExtra("content", thingsBean.remThings);
+                        intent.setFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+                        intent.setFlags(Intent.FLAG_RECEIVER_FOREGROUND);//设置可以给后台应用发广播
+                        intent.setComponent(name);
+                        String strId = String.valueOf(id);
+                        int intId = Integer.parseInt(strId);
+                        AlarmManager alarm = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+                        PendingIntent pendingIntent = PendingIntent.getBroadcast(mContext,
+                                intId, intent, PendingIntent.FLAG_MUTABLE);
+                        alarm.cancel(pendingIntent);
                         mHandler.post(new Runnable() {
                             @Override
                             public void run() {
